6bd354a667e90ca3fd8df921c1518207a210cc22
Grégory De Pass <33628995+gde-pass@users.noreply.github.com>
2018-11-15 20:52:10 +0100

    Struct presque finie

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 "use strict";
 
 const express = require('express');
-const app = express();
 const fs = require('fs');
-const port = 8080;
+const mysql = require("mysql");
 
-const io = require('socket.io').listen(app.listen(port));
+const app = express();
+const portAPP = 8080;
+const portSQL = 3306;
+const host = "127.0.0.1";
+
+const con = mysql.createConnection({
+    host: host,
+    port: portSQL,
+    user: "root",
+    password: "",
+    //database: ""
+});
+
+// con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected to the server mysql at %s:%s !", host, portSQL);
+// });
+
+const io = require('socket.io').listen(app.listen(portAPP, host, function (err) {
+    if (err) throw err;
+    console.log("Matcha is running at http://%s:%s", host, portAPP);
+}));
 
 app.get('/', function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
@@ -13,10 +33,8 @@ app.get('/', function (req, res) {
     res.end();
 });
 
-app.use(express.static(__dirname + '/app/public'));
-console.log("Matcha is running at http://localhost:" + port);
 
 io.sockets.on("connection", function (socket) {
 
 
-})
+});
