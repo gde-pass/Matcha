3483cfffe4e4fee6569d29272282c7b082aaddc9
gde-pass <gde-pass@student.42.fr>
2018-12-02 18:01:51 +0100

    Norme NODE

--- app/public/js/login_signup.js
+++ app/public/js/login_signup.js
@@ -2,38 +2,38 @@ const socket = io.connect("http://localhost:8080");
 
 // ============ FRONT EVENTS ===========
 
-function cambiar_login() {
+function cambiarLogin() {
     document.querySelector(".cont_forms").className = "cont_forms cont_forms_active_login";
     document.querySelector(".cont_form_login").style.display = "block";
     document.querySelector(".cont_form_sign_up").style.opacity = "0";
 
-    setTimeout(function(){  document.querySelector(".cont_form_login").style.opacity = "1"; },400);
+    setTimeout(function () {
+        document.querySelector(".cont_form_login").style.opacity = "1";
+    }, 400);
 
-    setTimeout(function(){
+    setTimeout(function () {
         document.querySelector(".cont_form_sign_up").style.display = "none";
-    },200);
+    }, 200);
 }
 
-function cambiar_sign_up() {
+function cambiarSignUp() {
     document.querySelector(".cont_forms").className = "cont_forms cont_forms_active_sign_up";
     document.querySelector(".cont_form_sign_up").style.display = "block";
     document.querySelector(".cont_form_login").style.opacity = "0";
 
-    setTimeout(function(){  document.querySelector(".cont_form_sign_up").style.opacity = "1";
-    },100);
+    setTimeout(function () {
+        document.querySelector(".cont_form_sign_up").style.opacity = "1";
+    }, 100);
 
-    setTimeout(function(){   document.querySelector(".cont_form_login").style.display = "none";
-    },400);
+    setTimeout(function () {
+        document.querySelector(".cont_form_login").style.display = "none";
+    }, 400);
 }
 
-function eventFire(el, etype){
-    if (el.fireEvent) {
-         el.fireEvent("on" + etype);
-    } else {
-        let evObj = document.createEvent("Events");
-        evObj.initEvent(etype, true, false);
-        el.dispatchEvent(evObj);
-    }
+function eventFire(el, etype) {
+    let evObj = document.createEvent("Events");
+    evObj.initEvent(etype, true, false);
+    el.dispatchEvent(evObj);
 }
 
 // ============ /FRONT EVENTS ===========
