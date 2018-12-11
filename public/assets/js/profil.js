59d77bfdf835ef3bab277112077e6b16a65425bf
gde-pass <gde-pass@student.42.fr>
2018-12-11 20:22:10 +0100

    A lot of pattern check added And some SWAL error message

--- public/assets/js/profil.js
+++ public/assets/js/profil.js
@@ -26,6 +26,62 @@ function updateTextAge(val) {
 
 // ============ CHECK FUNCTIONS ===========
 
+
+/**
+ * @return {boolean}
+ */
+
+function checkTagsPattern(value) {
+    const tagsRegex = new RegExp("^(#+[a-zA-Z]{2,20}\\s?){1,10}");
+
+    if (!value.match(tagsRegex)) {
+        return (false);
+    } else {
+        return (true);
+    }
+
+}
+
+/**
+ * @return {boolean}
+ */
+function checkBioPattern(value) {
+    //const bioRegex = new RegExp("");
+
+    if (value.length < 50 || value.length > 500) {
+        return (false);
+    } else {
+        return (true);
+    }
+}
+
+
+/**
+ * @return {boolean}
+ */
+function checkSexPattern(value) {
+    const sexRegex = new RegExp("^([OFM])$");
+
+    if (value.length !== 1 || !value.match(sexRegex)) {
+        return (false);
+    } else {
+        return (true);
+    }
+}
+
+/**
+ * @return {boolean}
+ */
+function checkSexOrientationPattern(value) {
+    const sexOrientationRegex = new RegExp("^(Ho|He|Bi)$");
+
+    if (value.length !== 2 || !value.match(sexOrientationRegex)) {
+        return (false);
+    } else {
+        return (true);
+    }
+}
+
 /**
  * @return {boolean}
  */
@@ -96,26 +152,87 @@ function checkPasswordMatch(password1, password2) {
 // ============ /CHECK FUNCTIONS =============
 
 
-
 // ============ SOCKET EVENTS =============
 socket.emit("getCookie", Cookies.get("token"));
 
 
 document.getElementById("save").addEventListener("click", function () {
-    document.getElementById("save").addEventListener("click", function () {
-        let first_name = document.getElementById("first_name").value;
-        let last_name = document.getElementById("last_name").value;
-        let sex = document.getElementById("sex").value;
-        let orientation = document.getElementById("orientation").value;
-        let age = document.getElementById("age").value;
-        let email = document.getElementById("email").value;
-        let tags = document.getElementById("tags").value;
-        let location = document.getElementById("location").value;
-        let bio = document.getElementById("bio").value;
-        let password = document.getElementById("password").value;
-        let password2 = document.getElementById("password2").value;
+    let first_name = document.getElementById("first_name").value;
+    let last_name = document.getElementById("last_name").value;
+    let username = document.getElementById("username").value;
+    let sex = document.getElementById("sex").value;
+    let orientation = document.getElementById("orientation").value;
+    let age = document.getElementById("age").value;
+    let email = document.getElementById("email").value;
+    let tags = document.getElementById("tags").value;
+    let distance = document.getElementById("distance").value;
+    let bio = document.getElementById("bio").value;
+    let password = document.getElementById("password").value;
+    let password2 = document.getElementById("password2").value;
 
-
+    if (first_name.length !== 0 && !checkName(first_name)) {
+        document.getElementById("first_name").style.borderColor = "red";
+        document.getElementById("first_name").style.borderStyle = "inset";
+        swal({
+            type: "error",
+            title: "`" + first_name + "` is not a valid first name",
+            html: "Please use a correct first name syntax: only <b>alphabetic characters</b> with the first one <b>uppercase</b>",
+        });
+    } else if (last_name.length !== 0 && !checkName(last_name)) {
+        document.getElementById("last_name").style.borderColor = "red";
+        document.getElementById("last_name").style.borderStyle = "inset";
+        swal({
+            type: "error",
+            title: "`" + last_name + "` is not a valid last name",
+            html: "Please use a correct last name syntax: only <b>alphabetic characters</b> with the first one <b>uppercase</b>",
+        });
+    } else if (username.length !== 0 && !checkUserPattern(username)) {
+        document.getElementById("username").style.borderColor = "red";
+        document.getElementById("username").style.borderStyle = "inset";
+        swal({
+            type: "error",
+            title: "`" + username + "` is not a valid username",
+            html: "Your username must contain between <b>2</b> and <b>15</b> characters with <b>alphabet</b> and <b>numbers</b> only."
+        });
+    } else if (email.length !== 0 && !checkEmailPattern(email)) {
+        document.getElementById("email").style.borderColor = "red";
+        document.getElementById("email").style.borderStyle = "inset";
+        swal({
+            type: "error",
+            title: "`" + email + "` is not a valid email",
+            html: "Please use a correct email syntax: <b>`exemple@domain.com`</b>",
+        });
+    } else if (orientation.length !== 0 && !checkSexOrientationPattern(orientation)) {
+        swal({
+            type: "error",
+            title: "WTF ?",
+            html: "Don't try to glitch bro",
+        });
+    } else if (sex.length !== 0 && !checkSexPattern(sex)) {
+        swal({
+            type: "error",
+            title: "WTF ?",
+            html: "Don't try to glitch bro",
+        });
+    } else if (bio.length !== 0 && !checkBioPattern(bio)) {
+        document.getElementById("bio").style.borderColor = "red";
+        document.getElementById("bio").style.borderStyle = "inset";
+        swal({
+            type: "error",
+            title: "Invalid bio",
+            html: "Your bio must contain between <b>50</b> and <b>500</b> characters."
+        });
+    } else if (tags.length !== 0 && !checkTagsPattern(tags)) {
+        document.getElementById("bio").style.borderColor = "red";
+        document.getElementById("bio").style.borderStyle = "inset";
+        swal({
+            type: "error",
+            title: "Invalid tags",
+            html: "Your tags must begin by a <b>'#'</b> they have to contain between <b>2</b> and <b>20</b> characters only and must be separate by a <b>space</b>."
+        });
+    } else if (!checkDistancePattern) {
+        //todo BIENTOT FINI
+    } else {
         socket.emit("parametre", {
             first_name: first_name,
             last_name: last_name,
@@ -124,11 +241,11 @@ document.getElementById("save").addEventListener("click", function () {
             orientation: orientation,
             age: age,
             tags: tags,
-            location: location,
+            distance: distance,
             bio: bio,
             password: password,
             password2: password2,
         });
-    });
+    }
 });
 // ============ /SOCKET EVENTS =============
